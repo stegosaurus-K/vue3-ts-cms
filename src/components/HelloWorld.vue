8324dd194ba9b043b2ed8ef5b5c7d909ac4a8612
jianlong.wei <331851471@qq.com>
2023-02-17 20:26:15 +0800

    fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -97,24 +97,12 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
-
-
-
-
 export default defineComponent({
   name: "HelloWorld",
-
-
-
 
   props: {
     msg: String,
   },
-
-
-
-
-  
 });
 </script>
 
