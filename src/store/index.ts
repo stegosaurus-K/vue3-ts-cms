d7e3c92c2c2f02bfbe0fa7272cd1efc92f279b29
jianlong.wei <331851471@qq.com>
2023-02-17 22:05:40 +0800

    项目环境配置

---
+++ src/store/index.ts
@@ -0,0 +1,13 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+  state: () => {
+    return {
+      name: "wjl",
+    };
+  },
+  mutations: {},
+  actions: {},
+});
+
+export default store;
