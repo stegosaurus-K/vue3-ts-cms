223721dcd33a74d74ff83fc98d15bb0364283ea0
jianlong.wei <331851471@qq.com>
2023-02-17 20:25:29 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,10 @@ import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
   name: "App",
-
-
-  
   components: {
     HelloWorld,
   },
 });
-
-
-
 </script>
 
 <style lang="less">
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -97,11 +97,24 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
+
+
+
+
 export default defineComponent({
   name: "HelloWorld",
+
+
+
+
   props: {
     msg: String,
   },
+
+
+
+
+  
 });
 </script>
 
